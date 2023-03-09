6e9fac66917a92489dfbee759bf0ef02e9e9be66
Felipe Nicolas Ribeiro Isidoro <15212348684@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-09 11:29:48 -0300

    Aula Matriz

---
+++ aula9/matriz.php
@@ -0,0 +1,29 @@
+<h2>Matriz</h2>
+<?php
+$produto1=[
+    "nome"=>"Notebook",
+    "marca"=>"Dell",
+    "preco"=>5000
+];
+$produto2=[
+    "nome"=>"Camera",
+    "marca"=>"Nikon",
+    "preco"=>3000
+];
+$produto3=[
+    "nome"=>"SmartPhone",
+    "marca"=>"LG",
+    "preco"=>1200
+];
+$tabela=[
+    $produto1,
+    $produto2,
+    $produto3
+];
+var_dump($tabela);
+echo $tabela[1]["marca"];
+echo "<br><br><br><br>";
+//json- JavaScript Object Notation
+echo json_encode($tabela);
+?>
+
