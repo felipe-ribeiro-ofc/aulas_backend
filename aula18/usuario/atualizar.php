0afd0f16e3fe3435ef2951d6efcfa8ffc13ffaaa
Felipe Ribeiro <felipe.ribeiro@alunos.ifsuldeminas.edu.br>
2023-05-22 08:41:18 -0300

    template

---
+++ aula18/usuario/atualizar.php
@@ -0,0 +1,24 @@
+<?php
+require_once "../conexao.php";
+if (isset($_POST["nome"]) && isset($_POST["login"]) && isset($_POST["senha"])) {
+   require_once "salvar_foto.php";
+   $id=$_POST["id"];
+   $nome = $_POST["nome"];
+   $login = $_POST["login"];
+   $senha = password_hash($_POST["senha"], PASSWORD_DEFAULT);
+   $foto = $nome_arquivo;
+
+   //String com o comando SQL para ser executado no DB
+   $sql = "UPDATE usuario SET `login`=?, `senha`=?, `nome`=?, `foto`=? WHERE  `idusuario`=?;";
+
+   //Prepara o SQL para ser executado no banco de dados
+   $comando = $conexao->prepare($sql);
+
+   //adiciona os valores nos parâmetros
+   $comando->bind_param("ssssi", $login, $senha, $nome, $foto, $id);
+
+   //executa o SQL - Comando no Banco de Dados
+   $comando->execute();
+}
+//abre o arquivo form.php
+header("Location: usuarios.php");
---
+++ aula18/usuario/usuarios.php
@@ -0,0 +1,45 @@
+<?php require_once "consultar_todos.php" ?>
+<?php require_once "../template/cabecalho.php" ?>
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <div class="container">
+        <h1>Usuários</h1>
+        <hr>
+        <div class="text-end">
+            <a href="form.php" class="btn btn-success">Inserir usuário</a>
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Login</th>
+                    <th scope="col">Foto</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($usuarios as $usuario) { ?>
+                    <tr>
+                        <th scope="row"><?php echo $usuario['nome'] ?></th>
+                        <td><?php echo $usuario['login'] ?></td>
+                        <td><img src="../uploads/<?php echo $usuario['foto'] ?>" height="30px" alt=""></td>
+                        <td>
+                            <a href="excluir.php?id=<?php echo $usuario['idusuario']; ?>" class="btn btn-danger"><i class="fa-solid fa-trash-can"></i>Excluir</a>
+                            <a href="form.php?id=<?php echo $usuario['idusuario']; ?>" class="btn btn-primary"><i class="fa-solid fa-pen-to-square"></i>Atualizar</a>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+    <?php require_once "../template/rodape.php" ?>
